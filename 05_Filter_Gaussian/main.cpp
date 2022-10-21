7ee908ef90e0f1dde0158e515a10ecd78f28b7fb
Jaejun Lee <jaejun.lees@gmail.com>
2022-10-21 10:12:08 +0900

    Add bigger image & check performance

--- 05_Filter_Gaussian/main.cpp
+++ 05_Filter_Gaussian/main.cpp
@@ -4,14 +4,26 @@
 using namespace cv;
 using namespace std;
 
+int64 t0;
+int64 t1;
+#define TBEGIN t0 = cv::getTickCount();
+#define TEND t1 = cv::getTickCount();
+#define TRES cout << "Elapsed time : " << (t1 / t0) * 1000 / getTickFrequency() << endl;
+
 int main()
 {
 	Mat src = imread("..\\Data\\lenna.bmp", IMREAD_GRAYSCALE);
 	Mat dst = Mat(src.size(), src.type());
 
-	int sigmasize = 5;
-	GaussianBlur(src, dst, Size(), (double)sigmasize); // sigma size must be odd number!!
-	String text = format("sigma = %d", sigmasize);
+	int sigma = 9;
+	TBEGIN;
+	Mat gauss = getGaussianKernel(8 * sigma + 1, sigma, CV_32FC1); //ksize = 8 * sigma + 1
+	GaussianBlur(src, dst, Size(), (double)sigma); // sigma size must be odd number!!
+	cout << "--Gaussian Kernerl--" << endl;
+	//cout << gauss;
+	TEND;
+	TRES;
+	String text = format("sigma = %d", sigma);
 	putText(dst, text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1.0, LINE_AA);
 
 	imshow("src", src);
